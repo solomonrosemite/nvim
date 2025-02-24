bee655d1376cdcf1d8b6425865054fa22bfb9f90
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2025-02-24 19:48:14 +0100

    feat: add avante

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,1 +1,12 @@
-return { 'numToStr/Comment.nvim', cond = vim.g.vscode == nil, opts = {} }
+return {
+  { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  {
+    'numToStr/Comment.nvim',
+    cond = vim.g.vscode == nil,
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
+  },
+}
---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/dressing.nvim',
+  opts = {
+    input = {
+      start_mode = 'normal',
+    },
+  },
+}
