4c23d9f495e97b34252c3dbae84117eda9fc4d31
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2026-01-25 20:21:48 +0100

    fix: leap plugin

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,8 @@
 return {
-  'ggandor/leap.nvim',
+  'https://codeberg.org/andyg/leap.nvim',
   event = 'BufRead',
   config = function()
-    require('leap').add_default_mappings()
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
   end,
 }
